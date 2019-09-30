4cf60af343beaec3c0890b3c31dfd3daa5162a37
Elizabeth Fuenzalida <efuenzalida52@gmail.com>
2019-09-29 21:56:52 -0700

    Started Project
    Startered tinkering with React project

--- fuenzalida_portfolio/src/App.js
+++ fuenzalida_portfolio/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RegularClass{}
+class ComponentClass extends Component{}
+
+const regularClassInstance = new RegularClass();
+const componentClassInstance = new ComponentClass();
+
+console.log('regularClassInstance', regularClassInstance);
+console.log('componentClassInstance', componentClassInstance);
+
+
+class App extends Component{
+    render(){
+        return(
+            <div> 
+                <h1>Hello World!</h1>
+                <p>
+                    My name is Elizabeth ~ I'm a software developer.
+                </p>
+                <p>
+                    I am looking forward to more meaningful projects.
+                </p>
+            </div>
+        )
+    }
 }
 
 export default App;
--- fuenzalida_portfolio/src/index.js
+++ fuenzalida_portfolio/src/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Parent Class
+class Animal{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    speak() {
+        console.log('I am ', this.name, 'and I am ', this.age, 'years old')
+    }
+}
+//providing arguments for parameters  and calling function
+const animal1 = new Animal('Simba', 3);
+animal1.speak();
+//debugging, showing data passed
+console.log(animal1);
+
+// Child class
+class Lion extends Animal{ 
+    // inheriting name & age from parent class
+    constructor(name, age, furColor, speed){ 
+        super(name,age)
+        this.furColor = furColor;
+        this.speed = speed;
+    }
+    // new function
+    roar(){
+        console.log('ROAR! I have a',
+        this.furColor, 
+        'mane',
+        'and can run ',
+         this.speed, 
+         'mph.');
+    }
+
+}
+// passing arguments for both local & inherited parameters
+const loin1 = new Lion('mufasa',20,'gold',60);
+loin1.speak();
+loin1.roar();
+console.log(loin1);
